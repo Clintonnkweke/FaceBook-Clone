93fc74a6489d0d7e32d416762cd04756e97fa879
Clintonnkweke <112875259+Clintonnkweke@users.noreply.github.com>
2023-02-28 21:50:56 +0100

    work_in_progress

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,3 +1,59 @@
-exports.home = (req, res) => {
-  res.send("Hello World clinton home");
+const { validateEmail, validateLength } = require("../helpers/validation");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+exports.register = async (req, res) => {
+  try {
+    const {
+      first_name,
+      last_name,
+      email,
+      username,
+      password,
+      bYear,
+      bMonth,
+      bDay,
+      gender,
+    } = req.body;
+
+    if (!validateEmail(email)) {
+      return res.status(400).json({ message: "Invalid email" });
+    }
+    const check = await User.findOne({ email });
+    if (check)
+      return res
+        .status(400)
+        .json({ message: "Email already exists, try different mail" });
+
+    if (!validateLength(first_name, 3, 30)) {
+      return res
+        .status(400)
+        .json({ message: "first name must between 3 and 30 character." });
+    }
+    if (!validateLength(last_name, 3, 30)) {
+      return res
+        .status(400)
+        .json({ message: "last name must between 3 and 30 character." });
+    }
+    if (!validateLength(password, 6, 40)) {
+      return res
+        .status(400)
+        .json({ message: "password must between 6 and 40 character." });
+    }
+    const cryptedPassword = await bcrypt.hash(password, 12);
+    console.log(cryptedPassword);
+    const user = await new User({
+      first_name,
+      last_name,
+      email,
+      username,
+      password,
+      bYear,
+      bMonth,
+      bDay,
+      gender,
+    }).save();
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
+const { readdirSync } = require("fs");
 dotenv.config();
 
 const app = express();
-const { readdirSync } = require("fs");
-
+app.use(express.json());
+app.use(cors());
+//routes
 readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
+
+//database
+mongoose
+  .connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+  .then(() => console.log("database connected successfully"))
+  .catch((err) => console.log("error connected to mongodb", err));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
